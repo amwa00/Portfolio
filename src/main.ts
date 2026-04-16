81be446833bd188072d7d902a15c64b92446819c
amwa00 <wallesamanda@gmail.com>
2026-04-16 13:39:28 +0200

    remove console.log

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 const navLinks = document.querySelectorAll(".nav__link");
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    console.log("You clicked on link");
     navLinks.forEach((l) => l.classList.remove("active"));
 
     link.classList.add("active");
